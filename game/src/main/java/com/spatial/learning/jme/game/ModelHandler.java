e40f7a353c4111dffaf468e2844d41ac18aff72e
Bhargav <69454005+BharSat@users.noreply.github.com>
2022-05-12 21:01:55 +0530

    Add Model Manager

--- game/src/main/java/com/spatial/learning/jme/game/ModelHandler.java
+++ game/src/main/java/com/spatial/learning/jme/game/ModelHandler.java
@@ -3,6 +3,11 @@ package com.spatial.learning.jme.game;
 public class ModelHandler {
     private SpatialLearningVWM app;
     ModelHandler (SpatialLearningVWM app) {
-        
+        this.app = app;
+    }
+
+    public void initialize() {
+        Node rootNode = this.app.getRootNode();
+        AssetManager assetManager = this.app.getAssetManager();
     }
 }
