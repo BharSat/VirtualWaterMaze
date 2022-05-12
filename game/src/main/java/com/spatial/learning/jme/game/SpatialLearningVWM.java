7236045abd294bbf99731be01886dd5038d6a9c7
BharSat <bhargavmbalaji@gmail.com>
2022-05-12 18:43:09 +0530

    Build the projects, and add build files

--- game/src/main/java/com/spatial/learning/jme/game/SpatialLearningVWM.java
+++ game/src/main/java/com/spatial/learning/jme/game/SpatialLearningVWM.java
@@ -1,19 +1,18 @@
 package com.spatial.learning.jme.game;
 
-import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
-import com.jme3.app.state.AppState;
 
-/**
- * The JMonkeyEngine game entry, you should only do initializations for your game here, game logic is handled by
- * Custom states {@link com.jme3.app.state.BaseAppState}, Custom controls {@link com.jme3.scene.control.AbstractControl}
- * and your custom entities implementations of the previous.
- *
- */
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppState;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.*;
+import com.jme3.scene.shape.*;
+
 public class SpatialLearningVWM extends SimpleApplication {
+    private BulletAppState physics;
+    private final float locations = 50.00f;
+    private final float ground = -1f;
 
     public SpatialLearningVWM() {
     }
@@ -24,14 +23,17 @@ public class SpatialLearningVWM extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        this.physics = new BulletAppState();
+        stateManager.attach(physics);
     }
+
+    public void initFloor() {
+        Box floorMesh = new Box(new Vector3f(0f-(locations/2), ground, 0f-(locations/2)), new Vector3f(0f+(locations/2), ground, 0f+(locations/2)));
+        Geometry floorGeom = new Geometry("floorGeom", floorMesh);
+        Material floorMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        floorMat.setTexture("floorTex", assetManager.loadTexture("Textures/Floor.png"));
+    }
+
+    public void initScene() {}
 
 }
