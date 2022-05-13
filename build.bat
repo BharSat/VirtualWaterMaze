d14300e1929ed13e31448601077fe62af0701f3c
BharSat <bhargavmbalaji@gmail.com>
2022-05-13 13:44:05 +0530

    add build .bat

---
+++ build.bat
@@ -0,0 +1,2 @@
+./gradlew :desktop:createJar
+"D:\Program Files\Android Studio\jre\bin\java.exe" -Dfile.encoding=windows-1252 -jar D:\bhargav\projects\VWMandroid\desktop\build\libs\desktop-1.0.0.jar
